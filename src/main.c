dd6ff263660ab54c2696d6bebd469bfb2bcb3c14
SadS4ndWiCh <hi@caiovinicius.xyz>
2026-03-05 06:15:23 -0300

    chore: update main example

--- src/main.c
+++ src/main.c
@@ -5,12 +5,12 @@
 #include "lexer.h"
 
 int main(void) {
-    char* src = "{\"hello\":\"world\"}";
+    char* src = "{\"hello\": \"world\", \"foo\": [\"bar\", true, false, 1353]}";
 
     lexer_t lex = { .src = src, .len = strlen(src) };
 
     for (token_t tok = lexer_next(&lex); tok.kind != TK_EOF; tok = lexer_next(&lex)) {
-        printf("kind = %d, literal = %s\n", tok.kind, tok.literal);
+        printf("kind = %s, literal = %s\n", token_str(tok.kind), tok.literal);
 
         if (tok.kind == TK_STRING) {
             free(tok.literal);
